88c1e10b76710094f419feab413bb767365bb855
Sampath” <Sampath@gmail.com>
2026-09-10 13:34:56 +0530

    Add Docker support and secure bcrypt configuration

--- backend/auth/auth.js
+++ backend/auth/auth.js
@@ -3,7 +3,7 @@ const passport = require('passport');
 const localStrategy = require('passport-local').Strategy;
 const User = require('../models/User')
 var bodyParser = require('body-parser')
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 
 
--- backend/routes/register.js
+++ backend/routes/register.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../models/User')
-var bcrypt = require('bcrypt');
+var bcrypt = require('bcryptjs');
 var moment = require('moment');
 var bodyParser = require('body-parser')
 
